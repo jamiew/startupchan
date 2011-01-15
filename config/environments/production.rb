efd2d6e7f25685a8897a87c39eba769546a0d37a
Jamie Wilkinson <jamie@jamiedubs.com>
2011-01-15 01:17:03 -0800

    Use sendmail and ignore bad emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,9 +35,11 @@ Bboard::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { :host => 'Bboard.com' }
+  # Use sendmail
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { :host => 'startupchan.com' }
 
   # Enable threaded mode
   # config.threadsafe!
