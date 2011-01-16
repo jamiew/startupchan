4967f62f213c56a80d2c872b3989150abef332a0
Jamie Wilkinson <jamie@jamiedubs.com>
2011-01-16 14:29:58 -0800

    Basic User spec yay

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should work with email" do
+    Factory.build(:user, :email => "rappers@internetofdeath.com").should be_valid
+  end
+
+  it "should fail without email" do
+    Factory.build(:user, :email => nil).should_not be_valid
+  end
+
+  it "should fail with an invalid email" do
+    ['bademail', 'bademail@example', 'bademail.com'].each do |email|
+      Factory.build(:user, :email => email).should_not be_valid
+    end
+  end
+
+  it "send a welcome email after create" do
+    ActionMailer::Base.deliveries.count.should == 0
+    Factory.create(:user, :email => "definitely@anewuser.com")
+    ActionMailer::Base.deliveries.count.should == 1
+  end
 end
