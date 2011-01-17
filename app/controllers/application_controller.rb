3d004db3a910fd2c71b1380c2fa001f5c071ee45
Jamie Wilkinson <jamie@jamiedubs.com>
2011-01-17 00:08:35 -0800

    Only 5 recent entries pls, thanks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,10 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
-
-  protected
+protected
 
   def get_sidebar_content
     @recent_threads = ForumThread.order('created_at DESC').limit(5)
-    @recent_entries = Entry.order('name DESC').limit(50)
+    @recent_entries = Entry.order('name DESC').limit(5)
   end
 end
