e618f89139da5f918e5d9c831ead8f1592ba0c7a
Jamie Wilkinson <jamie@jamiedubs.com>
2011-01-18 21:28:46 -0800

    Make link-click show metadata and unbind hover behavior

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -15,8 +15,7 @@ $(document).ready(function(){
     $('.flash').slideToggle('slow');
   }, 2000);
 
-
-  // Zebrafy table rows
+  // Zebrify table rows
   $("tr:even").css("background-color", "#000");
   $("tr:odd").css("background-color", "#111");
 
@@ -30,12 +29,13 @@ $(document).ready(function(){
     metaFor(this).hide();
   });
 
-  $('.post_content').click(function(){
-    // TODO show the meta & set it as "unhidable" with an attribute
-    // this gives us iPad/iPhone compatibility
+  // Show (and leave) meta controls on click... iOS doesn't have hover
+  // Unbind hover actions so mouse movements don't hide it  
+  $('.post_content a.text').click(function(){
+    $(this).parent().unbind('mouseenter mouseleave');
+    metaFor($(this).parent()).css('display', 'inline');
+    return false;
   });
 
-  // Hijax all remote links & forms
-  // TODO
 
 });
