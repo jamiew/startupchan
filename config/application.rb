67227ecb0a209f28162f6053a573e6b08514279c
Jamie Wilkinson <jamie@jamiedubs.com>
2011-10-26 09:52:20 -0700

    Tweak lib/exceptions include

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-require 'lib/exceptions'
-
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
@@ -48,3 +46,5 @@ module Bboard
     config.filter_parameters += [:password, :password_confirmation]
   end
 end
+
+require RAILS_ROOT+'/lib/exceptions'
