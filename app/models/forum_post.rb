58e5f0acb149d702ac863046a129a0764488e7bc
Jamie Wilkinson <jamie@jamiedubs.com>
2012-10-14 20:48:08 -0700

    Don't allow posts to contain links (SPAM BLOCK)

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -8,5 +8,14 @@ class ForumPost < ActiveRecord::Base
 
   validates_presence_of :text, :message => "text must not be blank"
   validates_length_of :text, :within => 2..1000, :on => :create, :message => "must be of sane length"
+  validate :does_not_contain_links
+
+protected
+
+  def does_not_contain_links
+    if self.comment =~ /http\:\/\//
+      self.errors.add(:login, 'cannot start with a number') if self.login && self.login =~ /^\d+/
+    end
+  end
 
 end
