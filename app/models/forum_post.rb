96ee1bafc3edf0d819621420a38db08882784f9d
Jamie Wilkinson <jamie@jamiedubs.com>
2013-09-23 23:30:57 -0700

    Fix Thread and Post validations
    Properly block http:// link spam

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -1,20 +1,21 @@
 class ForumPost < ActiveRecord::Base
+
+  validates_presence_of :text, :message => "text must not be blank"
+  validates_length_of :text, :within => 2..1000, :on => :create, :message => "must be a sane length"
+  validate :does_not_contain_links
+
   belongs_to :user
   belongs_to :forum_thread
   has_one :forum, :through => :forum_thread
-  # TODO belongs_to/trough? vs delegate for :forum association
 
   belongs_to :parent, :class_name => 'ForumPost'
-
-  validates_presence_of :text, :message => "text must not be blank"
-  validates_length_of :text, :within => 2..1000, :on => :create, :message => "must be of sane length"
-  validate :does_not_contain_links
 
 protected
 
   def does_not_contain_links
-    if self.text =~ /http\:\/\//
-      self.errors.add(:login, 'cannot start with a number') if self.login && self.login =~ /^\d+/
+    return if self.text.blank?
+    if self.text =~ /http(s)?\:\/\// || self.title =~ /\[url\]/
+      self.errors.add(:text, 'cannot contain links')
     end
   end
 
--- app/models/forum_thread.rb
+++ app/models/forum_thread.rb
@@ -1,4 +1,18 @@
 class ForumThread < ActiveRecord::Base
+
+  validates_presence_of :title
+  validate :does_not_contain_links
+
   belongs_to :forum
   has_many :forum_posts
+
+protected
+
+  def does_not_contain_links
+    return if self.title.blank?
+    if self.title =~ /http(s)?\:\/\// || self.title =~ /\[url\]/
+      self.errors.add(:text, 'cannot contain links')
+    end
+  end
+
 end
