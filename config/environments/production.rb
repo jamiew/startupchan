16670871d8cacea645fcf4ed7bc4049593f8b27f
Jamie Wilkinson <jamie@jamiedubs.com>
2013-09-24 00:03:40 -0700

    Use sendgrid for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,6 +40,17 @@ Bboard::Application.configure do
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.default_url_options = { :host => 'startupchan.com' }
 
+  # Send email using Sendgrid
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
   # Enable threaded mode
   # config.threadsafe!
 
